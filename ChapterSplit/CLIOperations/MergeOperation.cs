52fa3c06fea048f7a76f031107d242d86c4cee07
Tobias Reithmeier <tobias@nerdwana.net>
2020-02-04 13:16:40 +0100

    Merge into M4B files added

--- ChapterSplit/CLIOperations/MergeOperation.cs
+++ ChapterSplit/CLIOperations/MergeOperation.cs
@@ -1,7 +1,9 @@
 ﻿using ChapterSplit;
+using ChapterSplit.ffprobeOutput;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,10 @@ namespace ChapterSplit.CLIOperations
     internal class MergeOperation : ICLIOperation
     {
         private readonly string Verbose = "";
+        private string workingDir;
+        private Format formatDescriptions;
+        private double totalDuration = 0;
+
         private IEnumerable<string> SourceFiles { get; }
 
         public MergeOperation(IEnumerable<String> SourceFiles, bool verbose = false)
@@ -20,41 +26,101 @@ namespace ChapterSplit.CLIOperations
             {
                 this.Verbose = $"-hide_banner -loglevel panic -nostats";
             }
+
+            string filePath = $"{SourceFiles.ElementAt(0)}";
+            workingDir = Path.GetDirectoryName(filePath);
+
+            this.formatDescriptions = MediaHelper.GetMediaInfo(filePath).Format;
+            this.PrepareMetadataFile(this.formatDescriptions);
         }
 
         public void Run()
         {
-            // ffmpeg -i '.\1.m4b' -i '.\2.m4b' -filter_complex concat=n=2:v=0:a=1 -f mp4 -vn -y out.m4b
             Console.OutputEncoding = Encoding.UTF8;
             string Inputs = "";
+            var numberOfFiles = 0;
+
             foreach (var inputFile in this.SourceFiles)
             {
                 Console.WriteLine($"Processing: '{inputFile}'");
                 var serializedOutput = MediaHelper.GetMediaInfo(inputFile);
-                var chapters = serializedOutput.Chapters;
                 var formatDescriptions = serializedOutput.Format;
-                string filePath = $"{inputFile}";
-                var fileExtension = Path.GetExtension(filePath);
+
+                this.WriteMetadataFile(formatDescriptions);
 
                 Inputs += $"-i \"{inputFile}\" ";
-
-                // todo: set title to album
-                // todo: add chaptermarks
+                numberOfFiles += 1;
             }
 
-            var pExtract = new Process
+            Console.WriteLine($"Merging files into \"{this.formatDescriptions.Tags.Artist} - {this.formatDescriptions.Tags.Album}.m4b\"");
+
+            var pMerge = new Process
             {
                 StartInfo =
                             {
                                 UseShellExecute = false,
+                                WorkingDirectory = this.workingDir,
                                 FileName = $"{AppDomain.CurrentDomain.BaseDirectory}\\ffmpeg\\ffmpeg.exe",
-                                Arguments = $"{Inputs} -filter_complex concat=n=2:v=0:a=1 -map_metadata 0 -map_chapters 0 -vn -f mp4 -y {this.Verbose} target.m4b"
+                                Arguments = $"-i \".\\metadata.txt\" {Inputs}-filter_complex concat=n={numberOfFiles}:v=0:a=1 -map_metadata 0 -map_chapters 0 -vn -c:a libfdk_aac -vbr 3 -vn -movflags +faststart -f mp4 -y {this.Verbose} ~tmp.m4b"
                             }
             };
-            pExtract.Start();
-            // wait for end of extraction.
-            pExtract.WaitForExit();
-            pExtract.Dispose();
+            pMerge.Start();
+            pMerge.WaitForExit();
+            pMerge.Dispose();
+
+            var pMetadata = new Process
+            {
+                StartInfo =
+                            {
+                                UseShellExecute = false,
+                                WorkingDirectory = this.workingDir,
+                                FileName = $"{AppDomain.CurrentDomain.BaseDirectory}\\ffmpeg\\ffmpeg.exe",
+                                Arguments = $"-i \".\\~tmp.m4b\" -i \".\\cover.png\" -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title=\"Album cover\" -metadata:s:v comment=\"Cover (front)\" -y -f mp4 {this.Verbose} \"{this.formatDescriptions.Tags.Artist} - {this.formatDescriptions.Tags.Album}.m4b\""
+                            }
+            };
+            pMetadata.Start();
+            pMetadata.WaitForExit();
+            pMetadata.Dispose();
+            this.DeleteTmpFiles();
+        }
+
+        private void WriteLinesToFile(string[] lines)
+        {
+            var file = new System.IO.StreamWriter($"{this.workingDir}\\metadata.txt", true);
+            foreach (string line in lines)
+            {
+                file.WriteLine(line);
+            }
+            file.Close();
+        }
+
+        private void DeleteTmpFiles()
+        {
+            if (File.Exists($"{this.workingDir}\\metadata.txt"))
+            {
+                File.Delete($"{this.workingDir}\\metadata.txt");
+            }
+            if (File.Exists($"{this.workingDir}\\~tmp.m4b"))
+            {
+                File.Delete($"{this.workingDir}\\~tmp.m4b");
+            }
+        }
+
+        private void PrepareMetadataFile(Format formatDescriptions)
+        {
+            this.DeleteTmpFiles();
+            string[] lines = { ";FFMETADATA1", $"title={formatDescriptions.Tags.Album}", $"artist={formatDescriptions.Tags.Artist}", $"genre={formatDescriptions.Tags.Genre}", "" };
+            this.WriteLinesToFile(lines);
+        }
+
+        private void WriteMetadataFile(Format formatDescriptions)
+        {
+            var duration = Convert.ToDouble(formatDescriptions.Duration, new CultureInfo("en-US"));
+
+            string[] lines = { "[CHAPTER]", "TIMEBASE=1/1", $"START={this.totalDuration}", $"END={this.totalDuration + duration}", $"TITLE={formatDescriptions.Tags.Title}" };
+            this.WriteLinesToFile(lines);
+
+            this.totalDuration += duration;
         }
     }
 }
